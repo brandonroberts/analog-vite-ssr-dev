f000fcc5d8c304d26a7e28544e59037bcff73b5b
Brandon Roberts <robertsbt@gmail.com>
2022-11-22 22:06:12 -0600

    chore: reproduction of import.meta.glob error from npm package when using ssrLoadModule

--- src/entry-server.ts
+++ src/entry-server.ts
@@ -1,12 +1,16 @@
 import 'zone.js/node';
 import { renderApplication } from '@angular/platform-server';
 import { AppComponent } from './app/app.component';
+import { provideFileRouter } from '@analogjs/router';
 
 export default async function render(url: string, document: string) {
   const html = await renderApplication(AppComponent, {
     appId: 'analogApp',
     document,
-    url
+    url,
+    providers: [
+      provideFileRouter()
+    ]
   });
 
   return html;
